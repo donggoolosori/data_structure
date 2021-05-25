264501647484537828110f61b46b99a623bbd497
donggoolosori <kdj6551@naver.com>
2021-05-25 17:45:22 +0900

    Add dataset

--- Tree/binary_tree.cpp
+++ Tree/binary_tree.cpp
@@ -18,4 +18,44 @@ class Node {
   void setRight(Node<T>* right) { this - right = right; }
 };
 
-int main() { return 0; }
+template <typename T>
+void preOrder(Node<T>* node) {}
+
+template <typename T>
+void inOrder(Node<T>* node) {}
+
+template <typename T>
+void postOrder(Node<T>* node) {}
+
+int main() {
+  /*
+  아래 모양의 트리 생성
+              1
+            /    \
+          2        3
+        /  \     /   \
+      4     5   6     7
+    /  \
+  8      9
+  */
+  Node<int> root(1);
+  Node<int> n2(2);
+  Node<int> n3(3);
+  Node<int> n4(4);
+  Node<int> n5(5);
+  Node<int> n6(6);
+  Node<int> n7(7);
+  Node<int> n8(8);
+  Node<int> n9(9);
+
+  root.setLeft(&n2);
+  root.setRight(&n3);
+  n2.setLeft(&n4);
+  n2.setRight(&n5);
+  n3.setLeft(&n6);
+  n3.setRight(&n7);
+  n4.setLeft(&n8);
+  n4.setRight(&n9);
+
+  return 0;
+}
