834c805e2946d9c656eeec8bad9ea0ac945cbb49
donggoolosori <kdj6551@naver.com>
2021-05-28 23:05:42 +0900

    Add interpolation search

---
+++ Search/interpolation.cpp
@@ -0,0 +1,29 @@
+// 보간 탐색
+// 재귀
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int interpolation(vector<int> &arr, int low, int high, const int target) {
+  if (low > high) return -1;
+  int mid =
+      (double)(target - arr[low]) / (arr[high] - arr[low]) * (high - low) + low;
+  if (arr[mid] == target) {
+    return mid;
+  }
+  if (arr[mid] < target) {
+    return interpolation(arr, mid + 1, high, target);
+  } else {
+    return interpolation(arr, low, mid - 1, target);
+  }
+}
+
+int main() {
+  vector<int> arr = {1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51, 56, 61};
+  // 46의 위치 보간 탐색
+  int idx = interpolation(arr, 0, arr.size() - 1, 46);
+  // 출력 > 9
+  cout << idx << '\n';
+  return 0;
+}
