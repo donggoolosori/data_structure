8069238abe00fa7a49a4195d6eaf4ad9ea9280db
donggoolosori <kdj6551@naver.com>
2021-06-07 11:08:37 +0900

    Refactor

--- Search/binary_search_tree.cpp
+++ Search/binary_search_tree.cpp
@@ -13,12 +13,16 @@ class Node {
   Node(int key, int data) : key(key), data(data) {}
   int getKey() { return key; }
   void setKey(int key) { this->key = key; }
+
   int getData() { return data; }
   void setData(int data) { this->data = data; }
+
   void setLeft(Node* newNode) { left = newNode; }
   Node* getLeft() { return left; }
+
   void setRight(Node* newNode) { right = newNode; }
   Node* getRight() { return right; }
+
   ~Node() { delete this; }
 };
 
@@ -27,7 +31,6 @@ class Tree {
   Node* root;
 
  public:
-  Tree() : root(NULL) {}
   void insert(int key, int data) {
     Node* newNode = new Node(key, data);
     if (root == NULL) {
@@ -52,11 +55,15 @@ class Tree {
 
   void erase(int key) {
     if (root == NULL) return;
+
     Node* curr = root;
     Node* parent = NULL;
+
     while (curr != NULL) {
       parent = curr;
+
       if (curr->getKey() == key) break;
+
       if (curr->getKey() < key) {
         curr = curr->getRight();
       } else {
@@ -71,54 +78,66 @@ class Tree {
       } else {
         parent->setRight(NULL);
       }
+
       delete curr;
     }
     // 오른쪽 노드만 갖는경우
     else if (curr->getLeft() == NULL) {
       if (parent->getLeft() == curr) {
         parent->setLeft(curr->getRight());
+
         delete curr;
       } else {
         parent->setRight(curr->getRight());
+
         delete curr;
       }
       // 왼쪽 노드만 갖는경우
     } else if (curr->getRight() == NULL) {
       if (parent->getLeft() == curr) {
         parent->setLeft(curr->getLeft());
+
         delete curr;
       } else {
         parent->setRight(curr->getLeft());
+
         delete curr;
       }
     } else {
       Node* minParent = curr;
       Node* minNode = curr->getRight();
+
       while (minNode->getLeft() != NULL) {
         minParent = minNode;
         minNode = minNode->getLeft();
       }
+
       curr->setData(minNode->getData());
       curr->setKey(minNode->getKey());
+
       if (minParent->getLeft() == minNode) {
         minParent->setLeft(minNode->getRight());
       } else {
         minParent->setRight(minNode->getRight());
       }
+
       delete minNode;
     }
   }
 
   int get(int key) {
     Node* curr = root;
+
     while (curr != NULL) {
       if (curr->getKey() == key) return curr->getData();
+
       if (key < curr->getKey()) {
         curr = curr->getLeft();
       } else {
         curr = curr->getRight();
       }
     }
+
     return -1;
   }
   void clear(Node* root) {
@@ -126,6 +145,7 @@ class Tree {
 
     clear(root->getLeft());
     clear(root->getRight());
+
     delete root;
   }
   ~Tree() { clear(root); }
@@ -144,5 +164,6 @@ int main() {
   cout << tree->get(2004) << '\n';
 
   delete tree;
+
   return 0;
 }
