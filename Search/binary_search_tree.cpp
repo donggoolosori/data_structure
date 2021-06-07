f53ffcd2ed44f37061546581acb1af152f503e68
donggoolosori <kdj6551@naver.com>
2021-06-07 13:01:11 +0900

    Implement delete method

--- Search/binary_search_tree.cpp
+++ Search/binary_search_tree.cpp
@@ -13,11 +13,15 @@ class BST {
   BST(int);
   // insert function
   BST* insert(BST*, int);
+  // find min value BST node
+  BST* minValueNode(BST*);
+  // erase function
+  BST* erase(BST*, int);
   // Inorder traversal
   void inorder(BST*);
 };
 
-// BST::BST() : data(0), left(NULL), right(NULL) {}
+BST::BST() : data(0), left(NULL), right(NULL) {}
 
 BST::BST(int data) : data(data), left(NULL), right(NULL) {}
 
@@ -32,6 +36,61 @@ BST* BST::insert(BST* root, int data) {
     root->left = insert(root->left, data);
   }
 
+  return root;
+}
+
+BST* BST::minValueNode(BST* root) {
+  BST* ret = root;
+
+  while (ret && ret->left != NULL) {
+    ret = ret->left;
+  }
+
+  return ret;
+}
+
+BST* BST::erase(BST* root, int data) {
+  // base case
+  if (root == NULL) return root;
+
+  // if the data to be deleted is
+  // smaller than the root's data,
+  // then it lies in left subtree.
+  if (data < root->data) root->left = erase(root->left, data);
+
+  // if the data to be deleted is
+  // bigger than the root's data,
+  // then it lies in right subtree.
+  else if (data > root->data)
+    root->right = erase(root->right, data);
+
+  // if the data is same as root's data,
+  // then this is the node to be deleted.
+  else {
+    // 단말노드일 경우
+    if (root->left == NULL && root->right == NULL) {
+      return NULL;
+    }
+    // 오른쪽 자식노드만 존재할 경우
+    else if (root->left == NULL) {
+      BST* temp = root->right;
+      delete root;
+      return temp;
+    }
+    // 왼쪽 자식노드만 존재할 경우
+    else if (root->right == NULL) {
+      BST* temp = root->left;
+      delete root;
+      return temp;
+    }
+    // 왼쪽 오른쪽 자식노드 둘 다 존재할 경우
+    // find the smallest in the right subtree
+    BST* temp = minValueNode(root->right);
+    // copy
+    root->data = temp->data;
+    // delete
+    root->right = erase(root->right, temp->data);
+  }
   return root;
 }
 
@@ -53,6 +112,8 @@ int main() {
   bst.insert(root, 70);
   bst.insert(root, 60);
   bst.insert(root, 80);
+
+  bst.erase(root, 40);
 
   bst.inorder(root);
 
